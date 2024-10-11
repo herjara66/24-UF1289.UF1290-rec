35a72c67c477ccbbaad4d6a4dc34e11ad0839b2e
herjara66 <jaramillojesush@gmail.com>
2024-10-11 12:29:10 +0200

    se crea la clase person  con sus atributos id,name,email, se implementa la subclase Student,Profesor,Admnin.

--- config.js
+++ config.js
@@ -1,5 +1,32 @@
-/*const mysql = require("mysql");
+const mysql = require("mysql");
 require("dotenv").config();
+
+class Person {
+  constructor(id, name, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+}
+
+class Student extends Person {
+  constructor(id, name, email, score) {
+    super(id, name, email);
+    this.score = score;
+  }
+}
+
+class Professor extends Person {
+  constructor(id, name, email) {
+    super(id, name, email);
+  }
+}
+
+class Administrator extends Person {
+  constructor(id, name, email) {
+    super(id, name, email);
+  }
+}
 
 class Database {
   constructor() {
@@ -92,81 +119,4 @@ db.insertPerson("Alice Smith", "alice.smith@example.com", "student", 85)
     db.close();
   });
 
-module.exports = db;
-
-*/
-
-const mysql = require("mysql");
-require("dotenv").config();
-class Database {
-  constructor() {
-    this.connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-  }
-
-  connect() {
-    this.connection.connect((err) => {
-      if (err) {
-        console.error("Error connecting to MySQL:", err);
-        return;
-      }
-      console.log("Connected to MySQL!");
-    });
-  }
-
-  query(sql, args) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(sql, args, (err, results) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(results);
-      });
-    });
-  }
-
-  close() {
-    return new Promise((resolve, reject) => {
-      if (this.connection.state === "disconnected") {
-        return resolve();
-      }
-      this.connection.end((err) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  }
-
-  insertPerson(name, email, role, score) {
-    const sql =
-      "INSERT INTO persons (name, email, role, score) VALUES (?, ?, ?, ?)";
-    return this.query(sql, [name, email, role, score]);
-  }
-
-  getStudentById(id) {
-    const sql = "SELECT * FROM persons WHERE id = ? AND role = 'student'";
-    return this.query(sql, [id]);
-  }
-}
-
-const db = new Database();
-db.connect();
-
-db.getStudentById(1)
-  .then((student) => {
-    console.log(student);
-  })
-  .catch((err) => {
-    console.error("Error fetching student:", err);
-  })
-  .finally(() => {
-    db.close();
-  });
-
-module.exports = db;
+module.exports = { db, Student, Professor, Administrator };
